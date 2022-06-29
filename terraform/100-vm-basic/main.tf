a23e449f3e2d9980e3f6064717b10bbab91eb5d5
Peter van Grinsven <104399941+datacom-pvg@users.noreply.github.com>
2022-06-30 10:11:50 +1200

    terra-100-vm-basic - initial

---
+++ terraform/100-vm-basic/main.tf
@@ -0,0 +1,35 @@
+/* 
+-------------------------------------------------------------------------------------------
+Name: 100-vm-basic - main
+
+Description:  Setup of a basic, stand alone virtual machine in Datacom Cloud FlexCompute
+
+Dependencies: Existing Org, VDC, Edge Gateway, Network and VM Template
+-------------------------------------------------------------------------------------------
+*/
+
+# Connection to VDC within Org hosted at VMware Cloud Director URL
+provider "vcd" {
+  url      = var.vcd_apiurl
+  user     = var.vcd_user
+  password = var.vcd_pass
+  org      = var.vcd_org
+  vdc      = var.vcd_vdc
+}
+
+# VM based on specified VM template
+resource "vcd_vm" "vm" {
+  name          = var.vm_name
+  catalog_name  = var.template_catalog
+  template_name = var.vm_template
+  power_on      = "true"
+  memory        = var.vm_memory
+  cpus          = var.vm_cpus
+  cpu_cores     = var.vm_cpucores
+  network {
+    type               = var.vm_network_type
+    name               = var.vm_network_name
+    ip_allocation_mode = var.vm_ip_allocation_mode
+    ip                 = var.vm_ip
+  }
+}
---
+++ terraform/100-vm-basic/variable.tf
@@ -0,0 +1,64 @@
+/* 
+-------------------------------------------------------------------------------------------
+Name: 100-vm-basic - variables
+
+Description:  Setup of a basic virtual machine in Datacom Cloud FlexCompute
+
+Dependencies: Existing Org, VDC, Edge Gateway, Network and VM Template
+-------------------------------------------------------------------------------------------
+*/
+
+# VMware Cloud Director Provider variables
+variable "vcd_apiurl" {
+  description = "URL for the Cloud Director API endpoint"
+}
+variable "vcd_user" {
+  description = "Username for Cloud Director API operations"
+}
+variable "vcd_pass" {
+  description = "Password for Cloud Director API operations"
+}
+variable "vcd_org" {
+  description = "Cloud Director Org on which to run API operations"    
+}
+variable "vcd_vdc" {
+  description = "Virtual datacenter within Cloud Director to run API operations against"
+}
+
+# Virtual Machine variables
+variable "vm_name" {
+  description = "VM name"
+}
+variable "template_catalog" {
+  description = "Catalog name in which to find the given vApp Template"
+}
+variable "vm_template" {
+  description = "Name of the vApp Template to use"
+}
+variable "vm_memory" {
+  description = "Amount of RAM (in MB) to allocate to the VM"
+  default = 2048
+}
+variable "vm_cpus" {
+  description = "Number of virtual CPUs to allocate to the VM"
+  default = 1
+}
+variable "vm_cpucores" {
+  description = "Number of cores per socket / virtual CPU"
+  default = 1
+}
+variable "vm_network_type" {
+  description = "Network type, one of: `none`, `vapp` or `org`"
+  default = "org"
+}
+variable "vm_network_name" {
+  description = "Name of the network this VM should connect to"
+}
+variable "vm_ip_allocation_mode" {
+  description = "IP address allocation mode. One of `POOL`, `DHCP`, `MANUAL`, `NONE`"
+  default = "POOL"
+}
+variable "vm_ip" {
+  description = "Should be empty string for `POOL`, `DHCP` and NONE. For `MANUAL`, Value must be valid IP address from a subnet defined in `static pool` for network"
+  default = ""
+}
