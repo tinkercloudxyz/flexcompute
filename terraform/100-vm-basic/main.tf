bc6175ba9c1340be421176c6a1e5f18ca7b4427d
Peter van Grinsven <104399941+datacom-pvg@users.noreply.github.com>
2022-06-30 15:59:50 +1200

    Added computer_name attribute for VM

--- terraform/100-vm-basic/main.tf
+++ terraform/100-vm-basic/main.tf
@@ -20,6 +20,7 @@ provider "vcd" {
 # VM based on specified VM template
 resource "vcd_vm" "vm" {
   name          = var.vm_name
+  computer_name = var.vm_name
   power_on      = "true"
   memory        = var.vm_memory
   cpus          = var.vm_cpus
