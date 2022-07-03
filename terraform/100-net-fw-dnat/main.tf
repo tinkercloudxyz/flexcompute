ef31ab38d9d0b40effb74f827e87c3e8449834c8
Peter van Grinsven <104399941+datacom-pvg@users.noreply.github.com>
2022-07-03 15:45:18 +1200

    Main code setting up DNAT using providers and variables

---
+++ terraform/100-net-fw-dnat/main.tf
@@ -0,0 +1,36 @@
+/* 
+-------------------------------------------------------------------------------------------
+Name: 100-net-fw-dnat - main
+
+Description:  Setup of a firewall destination NAT for internal (on an Edge Gateway in 
+              Datacom Cloud FlexCompute
+
+Dependencies: Existing Org, VDC and Edge Gateway
+-------------------------------------------------------------------------------------------
+*/
+
+# Connection to VDC within Org hosted at VMware Cloud Director URL
+provider "vcd" {
+  url      = var.vcd_apiurl
+  user     = var.vcd_user
+  password = var.vcd_pass
+  org      = var.vcd_org
+  vdc      = var.vcd_vdc
+}
+
+# Destination NAT on Edge Gateway
+resource "vcd_nsxv_dnat" "dnat_1" {
+  edge_gateway = var.network_edge_gateway_name
+  network_type = var.network_type
+  network_name = var.network_name
+  enabled = var.network_dnat_enabled
+  description  = var.network_dnat_description
+  loggin_enabled = var.network_dnat_logging
+
+  original_address = var.network_dnat_orig_addr
+  original_port    = var.network_dnat_orig_port
+
+  translated_address = var.network_dnat_trans_addr
+  translated_port    = var.network_dnat_trans_port
+  protocol           = var.network_dnat_protocol
+}
